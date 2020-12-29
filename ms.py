def866c01f717182bb3c90651987e8638815a0bb
gamesguru <mathmuncher11@gmail.com>
2020-12-28 22:41:12 -0500

    wip ms

---
+++ ms.py
@@ -0,0 +1,21 @@
+
+def merge(input):
+    lines = input.split('\n')
+    n = int(lines[0])
+    arr = [int(x) for x in lines[1].split()]
+
+    i = n // 2
+    arr1 = arr[0:i]
+    arr2 = arr[i:n]
+
+    # TODO: implement custom sort method :)
+    arr1 = sort(arr1)
+    arr2 = sort(arr2)
+
+
+
+merge("""10
+20 19 35 -18 17 -20 20 1 4 4""")
+
+# with open('ms.txt') as ms:
+#     merge(ms.read())
