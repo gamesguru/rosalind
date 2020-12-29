156dfdb0c3efaeaa049b03f414102bdc306871da
gamesguru <mathmuncher11@gmail.com>
2020-12-28 21:00:35 -0500

    wip bins

--- bins.py
+++ bins.py
@@ -1,34 +1,39 @@
-
 def bin(input):
+    def index(e, A, p=None, q=None):
+        if p is None or q is None:
+            p = 0
+            q = len(A) - 1
 
-    def index(e, a):
-        n = len(a)
-        # k = 1
-        # i = n // 2 ** k
-
-        p = 1
-        q = n
-        while : # i > 0:
-            if e < a[i]:
-                k += 1
-                i = n // 2 ** k
-            elif e > a[i]:
-                k += 1
-                i = n // 2 ** k
+        if p == q:
+            if A[p] == e:
+                return p
+            else:
+                return -1
+        else:
+            i = (p + q) // 2
+            if e < A[i]:
+                return index(e, A, p=p, q=(i - 1))
+            elif e > A[i]:
+                return index(e, A, p=(i + 1), q=q)
             else:
                 return i
 
-    lines = input.split('\n')
+    lines = input.split("\n")
     n = int(lines[0])
     m = int(lines[1])
 
     sa = [int(x) for x in lines[2].split()]
     ua = [int(x) for x in lines[3].split()]
 
-    return [index(x, sa) for x in ua]
+    result = [index(x, sa) for x in ua]
+    print(result)
+    return result
 
-bin("""5
+
+bin(
+    """5
 6
 10 20 30 40 50
-40 10 35 15 40 20""")
+40 10 35 15 40 20"""
+)
 
