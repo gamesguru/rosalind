d09043934d197b4f7c2bb0c9fd6d22aaa19b1e99
gamesguru <mathmuncher11@gmail.com>
2021-01-02 11:06:06 -0500

    wip par3

--- par3.py
+++ par3.py
@@ -22,15 +22,30 @@ def par(input):
             a[0], a[r] = a[r], a[0]
             break
 
-    # l = 1
-    # while l < r:
-    #     for i in range(l, r):
-    #         if a[i]
+    l = 1
+    r = r
+    while l < r:
+        for j in range(r, l - 1, -1):
+            if a[j] != a0:
+                r = j
+                break
+        for i in range(l, r + 1):
+            if a[i] == a0:
+                l = i
+                break
+        if l < r:
+            a[l], a[r] = a[r], a[l]
+        else:
+            # exit
+            break
 
     print(a)
 
 
-par(
-    """9
-4 5 6 4 1 2 5 7 4"""
-)
+# par(
+#     """9
+# 4 5 6 4 1 2 5 7 4"""
+# )
+
+with open("/home/shane/Downloads/rosalind_par3.txt") as f:
+    par(f.read())
